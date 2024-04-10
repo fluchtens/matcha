7cacc791f10351b372c995027893091965a15df6
fluchtens <cleazy2002@gmail.com>
2024-04-10 10:31:11 +0200

    install express js

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,12 @@
-console.log("cacac");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Match API listening on port ${port}`);
+});
