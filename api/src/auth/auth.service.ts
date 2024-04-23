5add7375ce4f7d3f4d146e64c20c291b23e8a44c
fluchtens <cleazy2002@gmail.com>
2024-04-23 12:01:18 +0200

    add connection to database

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,6 +1,14 @@
 import { Response } from "express";
 
 export class AuthService {
+  async signup(body: any, res: Response): Promise<void> {
+    const username = body.username;
+    const password = body.password;
+
+    res.status(200);
+    res.send(`${username} - ${password}`);
+  }
+
   async login(body: any, res: Response): Promise<void> {
     const username = body.username;
     const password = body.password;
