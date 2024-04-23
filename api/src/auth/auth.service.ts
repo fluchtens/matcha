d5dc1174b89118311b2766448fc000f886789a33
fluchtens <cleazy2002@gmail.com>
2024-04-23 12:24:27 +0200

    add sql init

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -3,10 +3,13 @@ import { Response } from "express";
 export class AuthService {
   async signup(body: any, res: Response): Promise<void> {
     const username = body.username;
+    const email = body.email;
+    const firstName = body.first_name;
+    const lastName = body.last_name;
     const password = body.password;
 
     res.status(200);
-    res.send(`${username} - ${password}`);
+    res.send("User succesfully created.");
   }
 
   async login(body: any, res: Response): Promise<void> {
