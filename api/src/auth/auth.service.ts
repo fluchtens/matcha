4fbc2689d93ed3e5cdf565f1ed3c791f0e239ce6
fluchtens <cleazy2002@gmail.com>
2024-04-24 10:44:48 +0200

    fix init database and create user model

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Response } from "express";
 import { db } from "../database";
+import { UserModel } from "../user/user.model";
 
 export class AuthService {
+  private userModel: UserModel = new UserModel();
+
   async signup(body: any, res: Response): Promise<void> {
     const username = body.username;
     const email = body.email;
@@ -9,13 +12,22 @@ export class AuthService {
     const lastName = body.last_name;
     const password = body.password;
 
+    if (await this.userModel.getUserByUsername(username)) {
+      res.status(409).send("This username is already taken.");
+      return;
+    }
+
+    if (await this.userModel.getUserByEmail(email)) {
+      res.status(409).send("This email is already taken.");
+      return;
+    }
+
     const query =
       'INSERT INTO "user"(username, email, first_name, last_name, password) VALUES($1, $2, $3, $4, $5)';
     const values = [username, email, firstName, lastName, password];
     await db.query(query, values);
 
-    res.status(200);
-    res.send("User succesfully created.");
+    res.status(200).send("User succesfully created.");
   }
 
   async login(body: any, res: Response): Promise<void> {
---
+++ api/src/user/user.model.ts
@@ -0,0 +1,59 @@
+import { db } from "../database";
+
+type User = {
+  id: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+  bio: string;
+  password: string;
+};
+
+export class UserModel {
+  async getUserById(userId: number): Promise<User | null> {
+    try {
+      const query = 'SELECT * FROM "user" WHERE id = $1';
+      const values = [userId];
+      const result = await db.query(query, values);
+
+      if (result.rows.length <= 0) {
+        return null;
+      }
+      return result.rows[0];
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async getUserByUsername(username: string): Promise<User | null> {
+    try {
+      const query = 'SELECT * FROM "user" WHERE username = $1';
+      const values = [username];
+      const result = await db.query(query, values);
+
+      if (result.rows.length <= 0) {
+        return null;
+      }
+      return result.rows[0];
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async getUserByEmail(email: string): Promise<User | null> {
+    try {
+      const query = 'SELECT * FROM "user" WHERE email = $1';
+      const values = [email];
+      const result = await db.query(query, values);
+
+      if (result.rows.length <= 0) {
+        return null;
+      }
+      return result.rows[0];
+    } catch (error) {
+      return null;
+    }
+  }
+}
