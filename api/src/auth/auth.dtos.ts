c40e3c86f37e980f82e969fbec260ea4ecc4e122
fluchtens <cleazy2002@gmail.com>
2024-04-29 09:57:54 +0200

    use class validator to check signup body

---
+++ api/src/auth/auth.dtos.ts
@@ -0,0 +1,32 @@
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from "class-validator";
+
+export class SignUpDto {
+  @IsNotEmpty({ message: "Username cannot be empty." })
+  @IsString({ message: "Username must be a string." })
+  @Length(3, 16, {
+    message: "Username must be between 3 and 16 characters long.",
+  })
+  @Matches(/^[a-zA-Z0-9\-_]+$/, {
+    message: "Username can only contain letters, digits, underscores & hyphens.",
+  })
+  username: string;
+
+  @IsNotEmpty({ message: "Email cannot be empty." })
+  @IsString({ message: "Email must be a string." })
+  @IsEmail({}, { message: "Invalid email format." })
+  email: string;
+
+  @IsNotEmpty({ message: "First name cannot be empty." })
+  @IsString({ message: "First name must be a string." })
+  @Length(3, 50, {
+    message: "First name must be between 3 and 50 characters long.",
+  })
+  firstName: string;
+
+  @IsNotEmpty({ message: "Last name cannot be empty." })
+  @IsString({ message: "Last name must be a string." })
+  @Length(3, 50, {
+    message: "Last name must be between 3 and 50 characters long.",
+  })
+  lastName: string;
+}
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,56 +1,14 @@
 import bcrypt from "bcrypt";
+import { validateOrReject } from "class-validator";
 import { Response } from "express";
 import { db } from "../database";
 import { UserModel } from "../user/user.model";
+import { SignUpDto } from "./auth.dtos";
 
 export class AuthService {
   private userModel: UserModel = new UserModel();
 
-  checkUsername = (username: string) => {
-    if (username.length < 3 || username.length > 16) {
-      return { success: false, message: "Username must be between 3 and 16 characters long." };
-    }
-
-    const firstLetterRegex = /^[A-Za-z]/;
-    if (!firstLetterRegex.test(username)) {
-      return { success: false, message: "Username must start with a letter." };
-    }
-
-    const usernameRegex = /^(?=.*[a-zA-Z])[a-zA-Z0-9_-]+$/;
-    if (!usernameRegex.test(username)) {
-      return {
-        success: false,
-        message: "Username can only contain letters, digits, underscores & hyphens.",
-      };
-    }
-
-    return { success: true, message: null };
-  };
-
-  checkEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      return { success: false, message: "Email is invalid." };
-    }
-
-    return { success: true, message: null };
-  };
-
-  checkFirstName = (firstName: string) => {
-    if (firstName.length < 3 || firstName.length > 50) {
-      return { success: false, message: "First name must be between 3 and 50 characters long." };
-    }
-    return { success: true, message: null };
-  };
-
-  checkLastName = (lastName: string) => {
-    if (lastName.length < 3 || lastName.length > 50) {
-      return { success: false, message: "Last name must be between 3 and 50 characters long." };
-    }
-    return { success: true, message: null };
-  };
-
-  async signup(body: any, res: Response): Promise<void> {
+  async signup(body: any, res: Response) {
     const username = body.username ? body.username.trim() : "";
     const email = body.email ? body.email.trim() : "";
     const firstName = body.first_name ? body.first_name.trim() : "";
@@ -62,48 +20,34 @@ export class AuthService {
       return;
     }
 
-    const usernameCheck = this.checkUsername(username);
-    if (!usernameCheck.success) {
-      res.status(400).send(usernameCheck.message);
-      return;
-    }
+    try {
+      const signUpDto = new SignUpDto();
+      signUpDto.username = username;
+      signUpDto.email = email;
+      signUpDto.firstName = firstName;
+      signUpDto.lastName = lastName;
 
-    const emailCheck = this.checkEmail(email);
-    if (!emailCheck.success) {
-      res.status(400).send(emailCheck.message);
-      return;
-    }
-
-    const firstNameCheck = this.checkFirstName(firstName);
-    if (!firstNameCheck.success) {
-      res.status(400).send(firstNameCheck.message);
-      return;
-    }
-
-    const lastNameCheck = this.checkLastName(lastName);
-    if (!lastNameCheck.success) {
-      res.status(400).send(lastNameCheck.message);
-      return;
+      await validateOrReject(signUpDto);
+    } catch (error: any) {
+      const message = Object.values(error[0].constraints)[0];
+      return res.status(400).send(message);
     }
 
     if (await this.userModel.getUserByUsername(username)) {
-      res.status(409).send("This username is already taken.");
-      return;
+      return res.status(409).send("This username is already taken.");
     }
 
     if (await this.userModel.getUserByEmail(email)) {
-      res.status(409).send("This email is already taken.");
-      return;
+      return res.status(409).send("This email is already taken.");
     }
 
     const hashedPwd = await bcrypt.hash(password, 10);
-
     const query =
       'INSERT INTO "user"(username, email, first_name, last_name, password) VALUES($1, $2, $3, $4, $5)';
     const values = [username, email, firstName, lastName, hashedPwd];
     await db.query(query, values);
 
-    res.status(200).send("User succesfully created.");
+    return res.status(200).send("User succesfully created.");
   }
 
   async login(body: any, res: Response): Promise<void> {
