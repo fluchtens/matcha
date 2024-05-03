3681b2a27544946f01e05d3c856267081f2911da
Cleazyy <cleazy2002@gmail.com>
2024-05-03 13:27:40 +0200

    add login btn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,5 @@
 "use server";
 
-import LoginBtn from "@/app/LoginBtn";
-
 export default async function Home() {
-  return (
-    <main className="p-4">
-      <LoginBtn />
-    </main>
-  );
+  return <main className="p-4"></main>;
 }
